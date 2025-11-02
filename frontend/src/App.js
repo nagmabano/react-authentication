41acb411416ee3899a6dc2e9b72095842a861ee2
nagma <bano.nagma.1@gmail.com>
2025-11-03 00:56:21 +0530

    setting auth route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
+      {path: "auth", element: <AuthenticationPage />},
       { index: true, element: <HomePage /> },
       {
         path: 'events',
